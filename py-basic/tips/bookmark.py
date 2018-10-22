9b9c466f988125cd13c6b8c4d4196517cfaf3c04
Redwoods Yi <chaos21c@gmail.com>
2018-10-22 10:28:36 +0900

    wk09 upload

---
+++ py-basic/tips/bookmark.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+class Bookmark:
+    """
+    북마크 모듈
+    """
+
+    def __init__(self, title, url):
+        self.title = title
+        self.url = url
+
---
+++ py-basic/tips/testmodule.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import sys                # 표준 라이브러리를 임포트
+
+a = 1                     # 변수를 정의함
+b = "some string"
+
+def foo():                # 함수를 정의함
+    print("This is the function 'foo'")
+
+print("this is the top level") # 문자열을 표시함
+
+if __name__ == '__main__':
+    print("this is the code block")
