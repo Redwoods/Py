b6d8f76d0a6b9d32822948c953bae9d95b1595da
Sang Hoon Yi <chaos21c@gmail.com>
2019-09-09 09:35:04 +0900

    wk01

---
+++ py2019/Lec/code/wk01_ex01_hello.py
@@ -0,0 +1,24 @@
+a=1
+b=2
+c=a+b
+print(c)
+#################
+s1="Hello Python!"
+print(s1)
+#################
+x=[1,2,3]
+y=[4,5,6]
+print(x)
+print(x,y)
+x+y
+# function
+def add(a,b):
+    return a+b
+    
+#
+add(1,2)
+add(a,b)
+#################
+add(x,y)
+#################
+
