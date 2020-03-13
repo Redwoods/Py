0bf2e54c30d03bada5da85a516d5600474ba8095
Sang Hoon Yi <chaos21c@gmail.com>
2020-03-13 15:19:56 +0900

    Update ch01_hello.py

--- py2020/Code/ch1/ch01_hello.py
+++ py2020/Code/ch1/ch01_hello.py
@@ -2,15 +2,18 @@ a=1
 b=2
 c=a+b
 print(c)
+---
 #################
 s1="Hello Python!"
 print(s1)
 #################
+---
 x=[1,2,3]
 y=[4,5,6]
 print(x)
 print(x,y)
 x+y
+---
 # function
 def add(a,b):
     return a+b
