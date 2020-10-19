56aa3dae291c35e2c5e7e21675c4ec2d489c9ca2
Sang Hoon Yi <chaos21c@gmail.com>
2020-10-19 20:32:28 +0900

    Update diabetes-charts-app.py

--- pdm2020/my-note/py-streamlit/code/diabetes-charts-app.py
+++ pdm2020/my-note/py-streamlit/code/diabetes-charts-app.py
@@ -1,11 +1,19 @@
 import streamlit as st
 import pandas as pd
+
 # Get the data
 df = pd.read_csv("https://github.com/Redwoods/Py/raw/master/pdm2020/my-note/py-pandas/data/diabetes.csv")
+
 st.subheader('Data Information:')
 # Show the data as a table (you can also use st.write(df))
 st.dataframe(df)
 # Get statistics on the data
 st.write(df.describe())
+
 # Show the data as a chart.
 chart = st.line_chart(df)
+
+## mid-term practice
+## EDA of diabetes.csv
+# Your code here !!
+
